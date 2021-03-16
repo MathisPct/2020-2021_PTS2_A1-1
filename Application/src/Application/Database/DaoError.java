1f6ff400b38ae5fa017baff125d4d7be565d335d
MathisPct <math712b@gmail.com>
2021-03-16 23:49:36 +0100

    refactor: Add more constructor

--- Application/src/Application/Database/DaoError.java
+++ Application/src/Application/Database/DaoError.java
@@ -4,7 +4,34 @@ package Application.Database;
  * Exception générale de la couche DAO
  */
 public class DaoError extends Exception{
+    /**
+     *Crée une nouvelle instance de DaoError
+     */
     public DaoError(){
         super();
+    }
+
+    /**
+     * Crée une nouvelle instance de DaoError
+     * @param message Le message détaillant exception
+     */
+    public DaoError(String message){
+        super(message);
+    }
+
+    /**
+     * Crée une nouvelle instance de DaoError
+     * @param cause L'exception à l'origine de cette exception
+     */
+    public DaoError(Throwable cause) {
+        super(cause);
+    }
+    /**
+     * Crée une nouvelle instance de DaoError
+     * @param message Le message détaillant exception
+     * @param cause L'exception à l'origine de cette exception
+     */
+    public DaoError(String message, Throwable cause) {
+        super(message, cause);
     }
 }
