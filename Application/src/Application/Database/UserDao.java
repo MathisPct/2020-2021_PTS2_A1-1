bf7a5f78e1c39875291376577ac691154c2a8e8d
MathisPct <math712b@gmail.com>
2021-03-21 13:30:23 +0100

    Fix exception handle of feat 22-UserDao::Read()
    Une exception est maintenant levé lorsque un utilisateur n'est pas trouvé dans la BDD

--- Application/src/Application/Database/UserDao.java
+++ Application/src/Application/Database/UserDao.java
@@ -31,7 +31,7 @@ public class UserDao {
     }
 
     /**
-     * Lit un utilisateur depuis la BD. En cas d'erreur, lève une exception.
+     * Lit un utilisateur depuis la BD. En cas d'erreur, lève une exception de type DaoError.
      * @param aLogin login de l'utilisateur cherché
      * @param aPassHash valeur de hachage du mot de passe de l'utilisateur cherché
      * @return L'utilisateur chargé depuis la BD.
@@ -51,6 +51,10 @@ public class UserDao {
             System.out.println("Aucun user trouvé");
         }
         rSet.close();
+        //si aucun user n'a été trouvé on lève une exception
+        if (user == null){
+            throw new DaoError("Aucun utilisateur n'a été trouvé");
+        }
         return user;
     }
 
