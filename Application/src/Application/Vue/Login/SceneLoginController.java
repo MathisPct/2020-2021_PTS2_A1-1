3798f0476088fdd7814beea5f91281b7d42547cf
MathisPct <math712b@gmail.com>
2021-03-21 13:56:52 +0100

    add of things for connection() method

--- Application/src/Application/Vue/Login/SceneLoginController.java
+++ Application/src/Application/Vue/Login/SceneLoginController.java
@@ -6,20 +6,57 @@
 package Application.Vue.Login;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import Application.Database.DaoError;
+import Application.Database.UserDao;
+import Application.Main;
+import Application.Metier.User;
+import Application.Vue.UtilsIHM;
+import Application.Vue.main.MainController;
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author David
  */
 public class SceneLoginController implements Initializable {
+    /**
+     * Attribut chaine de caractère qui correspond au label "Identifiant" dans la scène
+     */
+    @FXML
+    private TextField idLabel;
+
+    /**
+     * Attribut chaine de caractère qui correspond au label "Mot de passe" dans la scène
+     */
+    @FXML
+    private TextField passwordLabel;
+
+    private UserDao dao;
+
+    private UtilsIHM utils = new UtilsIHM();
+
+    /**
+     * Utilisateur crée lors de la recherche d'un user dans la BDD avec read(login,mdp)
+     */
+    private User userConnected;
+
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         System.out.println("SceneLogin");
+        try {
+            dao = new UserDao();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     /**
@@ -32,4 +69,9 @@ public class SceneLoginController implements Initializable {
          * A compléter
          */
     }
+
+    public void setUserConnected(User user){
+        this.userConnected = user;
+    }
+
 }
